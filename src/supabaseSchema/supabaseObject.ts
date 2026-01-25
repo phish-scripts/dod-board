90a39bac108777053477b6db54e92f8b3c70175c
Samir Katakamsetty <katakamsettysamir@gmail.com>
2026-01-25 18:10:17 -0500

    project actually has supabase in it now

--- src/supabaseSchema/supabaseObject.ts
+++ src/supabaseSchema/supabaseObject.ts
@@ -1,7 +1,5 @@
-require("dotenv").config();
-
 import { createClient } from "@supabase/supabase-js";
-import { Database } from  "../types/supabase";
+import { Database } from  "@/types/supabase";
 
 export const supabase = createClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY as string);
 
