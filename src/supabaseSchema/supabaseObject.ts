40df0444ec8702a459e170511f90f50eca9433b7
Samir Katakamsetty <katakamsettysamir@gmail.com>
2026-01-25 18:00:37 -0500

    test commit

--- src/supabaseSchema/supabaseObject.ts
+++ src/supabaseSchema/supabaseObject.ts
@@ -3,10 +3,5 @@ require("dotenv").config();
 import { createClient } from "@supabase/supabase-js";
 import { Database } from  "../types/supabase";
 
-// check env
-const URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
-
-// initializing
 export const supabase = createClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY as string);
 
