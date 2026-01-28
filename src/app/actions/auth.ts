f5b8d714ee725d60cbec0ad95b0fff17bc0eb88f
Samir Katakamsetty <katakamsettysamir@gmail.com>
2026-01-27 20:24:05 -0500

    setting up the crawler

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -21,6 +21,7 @@ export async function login(formData: FormData)
 
     revalidatePath('/', 'layout')
     redirect('/dashboard')
-  
+
+    
 }
 
--- src/services/crawler/crawler.py
+++ src/services/crawler/crawler.py
@@ -1,5 +1,8 @@
-from bs4 import BeautifulSoup
+import requests
 
-with open("index.html") as fp:
-    soup = BeautifulSoup(fp, 'html.parser')
+def crawler():
+    response = requests.get("https://www.usajobs.gov/search/results/?k=It%20Data%20Analyst&p=1")
+    response.raise_for_status()
+    print(response.text)
 
+crawler()
