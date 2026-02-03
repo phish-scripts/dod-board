4064aecffdba7b46ee74673193dd6810daf6ba05
Samir Katakamsetty <katakamsettysamir@gmail.com>
2026-02-03 01:25:08 -0500

    getJobs and scrapedJobDetails work. fix: script doesn't scrape unrelated links, nor does it crash because 2 links are put together, causing a DNS lookup failure.

--- src/services/job_info/getJobs.py
+++ src/services/job_info/getJobs.py
@@ -5,6 +5,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 from selenium import webdriver
+from selenium_stealth import stealth
+import random
 
 from selenium.webdriver.chrome.options import Options
 
@@ -13,12 +15,29 @@ chrome_options = Options()
 chrome_options.add_argument("--headless") 
 driver = webdriver.Chrome(options=chrome_options)
  
+
+ban_list = ["https://www.usajobs.govhttps://help.usajobs.gov/faq/job-announcement/remote"]
+
+
 def getJobLinks(keyword):
     # change limit as needed
     page_number = 1
-    limit = 100
+    limit = 5
     driver = webdriver.Chrome(options = chrome_options)
+
+    # applying stealth so it doesn't get nailed
+    stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+    )
+
     job_links = set()
+
+
     print("Fetching jobs...")
     try:
         while(len(job_links) < limit):
@@ -27,6 +46,11 @@ def getJobLinks(keyword):
             driver.get(usaJobLink)
             time.sleep(2)
             print(f"scraping page {page_number}...")
+
+            # slowing the crawling and scraping
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight/2);")
+            time.sleep(random.uniform(2.5, 5.0))
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
             page = driver.page_source
             soup = BeautifulSoup(page, "html.parser")
@@ -37,16 +61,16 @@ def getJobLinks(keyword):
             for link in collected_a_elements:
                 var_href = link['href']
                 # checks to see if the link is actually a job link
-                if "/job" in var_href:
+                # kept scraping unrelated links
+                if "/job" in var_href and 'closing-types' not in var_href and 'faq' not in var_href:
                     # making sure that this link doesnt sneak its way into job_links
-                    if 'job-announcement/closing-types' not in var_href:
-                        new_url = "https://www.usajobs.gov" + var_href
-                        print("------------------")
+                    if 'job-announcement/closing-types' not in var_href and var_href not in ban_list: 
+                        # making sure if the URL needs to be cleaned
+                        new_url = var_href if var_href.startswith('http') else "https://www.usajobs.gov" + var_href
                         print(f"job found: {new_url}")
                         # checking to make sure that the new url isnt already in job_links, but i think set() already does that?
                         if new_url not in job_links:
                             print(f"Putting {new_url} in job_links")
-                            print("------------------------")
                             job_links.add(new_url)
                             found_on_page += 1
                     if len(job_links) >= limit:
@@ -60,7 +84,6 @@ def getJobLinks(keyword):
             page_number += 1 
            
         print("finished fetching jobs...")
-        print("total jobs found: ", len(job_links))
         return job_links
     
     finally:
--- src/services/job_info/scrapedJobDetails.py
+++ src/services/job_info/scrapedJobDetails.py
@@ -8,6 +8,9 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import re
+import random
+
+from selenium_stealth import stealth
 
 from getJobs import getJobLinks
 
@@ -19,7 +22,21 @@ def job_details(job_list):
     # remove GUI, dont need an actual chrome tab open each time
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    # Masking the automation flag in Chrome options
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+    
     driver = webdriver.Chrome(options=chrome_options)
+
+    # Apply Stealth Settings
+    stealth(driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+    )
 
     job_details_list = []
 
@@ -27,27 +44,38 @@ def job_details(job_list):
         try:
             print(f"getting details of {job_link}")
             driver.get(job_link)
-            time.sleep(2)
+            time.sleep(random.uniform(3.5, 6.2))
 
             page = driver.page_source
+
+            # lesson learned
+            if "Access Denied" in page:
+                print("Blocked from scraping. Darn.")
+                break
+
+
+
             soup = BeautifulSoup(page, "html.parser")
 
             # salary
             salary = soup.find('dt', string=lambda text: "Salary" in text)
             if salary:
                 salary_info = salary.find_next_sibling('dd').get_text(strip=True)
+            time.sleep(random.uniform(3.5, 6.2))
 
             # location
             location_container = soup.find(id="allLocations")
             if location_container:
                 location_elements = location_container.find_all("div", class_="font-bold")
                 location_list=[location.get_text(strip=True) for location in location_elements]
+            time.sleep(random.uniform(3.5, 6.2))
             
             # remote status
             remote = soup.find('dt', string=lambda text: "Remote job" in text)
             if remote:
                 remote_status = remote.find_next_sibling('dd').get_text(strip=True)
             pay_scale = soup.find(id="joaPayGrade").get_text(strip=True)
+            time.sleep(random.uniform(3.5, 6.2))
         
 
             # returning this job_data
@@ -55,11 +83,12 @@ def job_details(job_list):
                 "link": job_link,
                 "job_title": soup.find("h1").get_text(strip=True),
                 "where": soup.find("div", class_="uppercase usajobs-joa-banner__dept").get_text(strip=True),
-                "Salary": salary_info,
+                "Salary": salary_info if salary_info else "N/A",
                 "Location": location_list if location_list else "N/A",
-                "Remote": remote_status,
+                "Remote": remote_status if remote_status else "N/A",
                 "Pay Scale & Grade": pay_scale
             }
+            time.sleep(random.uniform(3.5, 6.2))
 
             print(job_data)
             job_details_list.append(job_data)
